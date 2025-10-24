d7d9ec7c4a263ffc6d37a949a78db0359ebfa3c7
aulia rahmi <auliarahmi.patria@gmail.com>
2025-10-24 14:37:11 +0700

    first commit seconf sommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Version Control!");
+        System.out.println("haiii yaya");
+        System.out.println("semester 3");
+        System.out.println("praktikum 3");
     }
 }
